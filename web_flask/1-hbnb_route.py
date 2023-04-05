cfe9c79a44f114391903c1a838b02c88a0925c24
johnNaduer <naduer789@gmail.com>
2023-04-05 15:29:04 -0700

    feat: add script that starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,10 +6,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
+
+
 def hello_hbnb():
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
+
+
 def hbnb():
     return 'HBNB'
 
