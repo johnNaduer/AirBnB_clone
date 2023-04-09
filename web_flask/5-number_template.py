c50009ce654ef6fa088d47073f525d1085ddc2bf
johnNaduer <naduer789@gmail.com>
2023-04-09 08:32:24 -0700

    feat: update code

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,8 +2,7 @@
 
 """ script that starts a Flask web application """
 
-from flask import Flask
-
+from flask import Flask, escape, render_template
 app = Flask(__name__)
 
 
@@ -31,6 +30,13 @@ def python(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     return '{} is a number'.format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_html(n):
+    """ function that returns a number"""
+    return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
